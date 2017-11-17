535a8d6f97c7d51b9114b9a955a990b0e50a9306
Matt Humphreys <mhumph@gmail.com>
2017-11-17 16:20:03 +0000

    Heroku ClearDB integration in progress

--- config/db-config.js
+++ config/db-config.js
@@ -1,13 +1,14 @@
 exports.getDbConfig = function() {
-	var host = process.env.ZEN_DB_HOST || 'localhost';
-	var user = process.env.ZEN_DB_USER || 'zenadmin';
-	var password = process.env.ZEN_DB_PASS || 'snowflake';
-	var database = process.env.ZEN_DB_SCHEMA || 'wikiboard';
+	// var host = process.env.ZEN_DB_HOST || 'localhost';
+	// var user = process.env.ZEN_DB_USER || 'zenadmin';
+	// var password = process.env.ZEN_DB_PASS || 'snowflake';
+	// var database = process.env.ZEN_DB_SCHEMA || 'wikiboard';
 
-	return {
-		'host'     : host,
-		'user'     : user,
-		'password' : password,
-		'database' : database
-	};
+	return process.env.CLEARDB_DATABASE_URL || 'mysql://zenadmin:snowflake@localhost/wikiboard';
+	// return {
+	// 	'host'     : host,
+	// 	'user'     : user,
+	// 	'password' : password,
+	// 	'database' : database
+	// };
 }
--- readme.txt
+++ readme.txt
@@ -1,2 +1,13 @@
 readme
 ======
+
+To setup with Heroku
+heroku git:remote -a zenbrd
+
+Info here
+https://dashboard.heroku.com/apps/zenbrd/deploy/heroku-git
+
+For DB
+heroku addons:create cleardb:punch
+
+(ClearDB free has more connections than JawsDB free)
